6e1d75883560a1ba76fe125ec96d087d7a53b43a
Michael Cabelin <ncabelin@gmail.com>
2017-08-13 14:34:53 -0700

    remove functionality add, color label add

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var app = express();
-
+var mongoose = require('mongoose');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 require('dotenv').config();
+mongoose.connect(process.env.MONGODB_URI);
+var stockSchema = new mongoose.Schema({
+  name: String
+});
+var Stock = mongoose.model('Stock', stockSchema);
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
   res.send('/index.html');
+});
+
+app.get('/list_stocks', function(req, res) {
+  Stock.find({}, function(err, stocks) {
+    if (err) {
+        console.log('Error : ' + err);
+        res.status(400).json({message:'Error finding stocks'})
+      } else {
+        res.status(200).json(stocks);
+      }
+  });
 });
 
 io.on('connection', function(socket) {
@@ -15,9 +32,32 @@ io.on('connection', function(socket) {
   socket.on('disconnect', function() {
     console.log('user disconnected');
   });
+
+  // add stock
   socket.on('add stock', function(stock) {
     console.log('adding ' + stock);
+    Stock.findOneAndUpdate({ name:stock }, {
+      name: stock
+    },{ upsert: true }, function(err, stk) {
+      if (err) {
+        console.log('Error : ' + err);
+      } else {
+        console.log('Recorded ' + stock);
+      }
+    });
     io.emit('add stock', stock);
+  });
+
+  // remove stock
+  socket.on('remove stock', function(stock) {
+    console.log('removing ' + stock);
+    Stock.findOneAndRemove({name:stock}, function(err) {
+      if (err) {
+        console.log('Error : ' + err);
+      } else {
+        io.emit('remove stock', stock);
+      }
+    });
   });
 });
 
