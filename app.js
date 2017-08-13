166597354bb4eb51ff19e8c4c76b96a3f89ab8da
Michael Cabelin <ncabelin@gmail.com>
2017-08-13 02:48:30 -0700

    move index.html to public folder

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
+
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 require('dotenv').config();
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/index.html');
+  res.send('/index.html');
 });
 
 io.on('connection', function(socket) {
